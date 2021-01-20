6bf143429f25e4eaa045ab396487805822f5525c
sagar <run2sagar@gmail.com>
2021-01-20 19:52:44 +0545

    Fixed Gallery Page JQuery

--- resources/views/display/header.blade.php
+++ resources/views/display/header.blade.php
@@ -9,11 +9,11 @@
 
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" />
     <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
     <script>
@@ -35,12 +35,6 @@
             });
 
         });
-        //navbar scroll effect
-        // $(window).scroll(function()
-        //       {
-        //
-        //         $('nav').toggleClass('scrolled',$(this).scrollTop()>50);
-        //       });
 
 
     </script>
